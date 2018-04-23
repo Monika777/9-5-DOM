593d4a12b4ae1fb2b2ebb855399a317c6f28042e
Monika Tarka <monikatarka@yahoo.co.uk>
2018-04-23 16:46:00 +0100

    Add index.html style.css and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var buttonClassItems = document.getElementsByClassName('button');
+
+for (var i = 0; i < buttonClassItems.length; i++) {
+    alert(buttonClassItems[i].innerText);
+    console.log(buttonClassItems[i].innerText)
+    }
